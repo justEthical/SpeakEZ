d7d9382f3c812be0c74291eb4f947127b681cd7a
justEthical <ammmm4507@gmail.com>
2025-06-20 21:38:26 +0530

    smalll fix

--- lib/Utils/custom_dialogs.dart
+++ lib/Utils/custom_dialogs.dart
@@ -4,6 +4,7 @@ import 'package:speak_ez/Constants/app_strings.dart';
 import 'package:speak_ez/Controllers/global_controller.dart';
 import 'package:speak_ez/Screens/Login/login_screen.dart';
 import 'package:speak_ez/Services/auth_service.dart';
+import 'package:speak_ez/Services/firestore_helper.dart';
 
 class CustomDialogs {
   static Widget logoutDialog() {
@@ -36,7 +37,7 @@ class CustomDialogs {
                   },
                   style: ElevatedButton.styleFrom(
                     shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(0),
+                      borderRadius: BorderRadius.circular(10),
                     ),
                   ),
                   child: const Text('Yes'),
@@ -49,7 +50,7 @@ class CustomDialogs {
                   style: ElevatedButton.styleFrom(
                     backgroundColor: Colors.deepPurple,
                     shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(0),
+                      borderRadius: BorderRadius.circular(10),
                     ),
                   ),
                   child: const Text(
@@ -83,14 +84,75 @@ class CustomDialogs {
             ),
             SizedBox(height: 10),
             ElevatedButton(
-              onPressed: (){
+              onPressed: () {
                 globalController.openAppSetting();
               },
               child: Text("Open Settings"),
-            )
+            ),
           ],
         ),
       ),
+    );
+  }
+
+  static Widget deleteConfirmationDialog() {
+    return Dialog(
+      child: Container(
+        padding: const EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text(
+              'Are you sure you want to delete your account?\n\nThis action is irreversible.',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              children: [
+                ElevatedButton(
+                  onPressed: () async{
+                    final res = await FirestoreHelper.deleteCurrentUser();
+                    if (res) {
+                      globalController.prefs?.setString(
+                        AppStrings.userProfile,
+                        "loggedOut",
+                      );
+                      Get.offAll(() => const LoginScreen());
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  child: const Text('Delete'),
+                ),
+                Spacer(),
+                ElevatedButton(
+                  onPressed: () {
+                    Get.back();
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.deepPurple,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  child: const Text(
+                    'Cancel',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      )
     );
   }
 }
