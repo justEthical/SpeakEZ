0455f14682323ad39dc4d058074df074b28cdc20
Ashok <80592078+justEthical@users.noreply.github.com>
2025-08-13 22:03:22 +0530

    fixed lessons not loading from lessons cdn (#6)
    * fixed lessons not loading from lessons cdn
    
    * removed unused code
    
    * removed unused function

--- lib/Controllers/home_screen_controller.dart
+++ lib/Controllers/home_screen_controller.dart
@@ -18,39 +18,6 @@ class HomeScreenController extends GetxController {
   void onReady() {
     // TODO: implement onReady
     super.onReady();
-    // Future.delayed(Duration.zero, () async {
-    //   currentLessonNameList.value = await loadLessonsFromJson("A1");
-    // });
-  }
-
-  void changeEnglishLevel(String level) async {
-    // switch (level) {
-    //   case "A1":
-    //     currenEnglishLessonLevel.value = "A1";
-    //     loadLessonsFromJson('assets/lessons/a1.json');
-    //     break;
-    //   case "A2":
-    //     currenEnglishLessonLevel.value = "A2";
-    //     loadLessonsFromJson('assets/lessons/a2.json');
-    //     break;
-    //   case "B1":
-    //     currenEnglishLessonLevel.value = "B1";
-    //     loadLessonsFromJson('assets/lessons/b1.json');
-    //     break;
-    //   case "B2":
-    //     currenEnglishLessonLevel.value = "B2";
-    //     loadLessonsFromJson('assets/lessons/b2.json');
-    //     break;
-    //   case "C1":
-    //     currenEnglishLessonLevel.value = "C1";
-    //     loadLessonsFromJson('assets/lessons/c1.json');
-    //     break;
-    //   case "C2":
-    //     currenEnglishLessonLevel.value = "C2";
-    //     loadLessonsFromJson('assets/lessons/c2.json');
-    //     break;
-    // }
-    currenEnglishLessonLevel.value = level;
   }
 
   void calculateStreak() {
@@ -74,22 +41,6 @@ class HomeScreenController extends GetxController {
     }
   }
 
-  final lessonListPath = {
-    'A1': 'assets/lessons/a1.json',
-    'A2': 'assets/lessons/a2.json',
-    'B1': 'assets/lessons/b1.json',
-    'B2': 'assets/lessons/b2.json',
-    'C1': 'assets/lessons/c1.json',
-    'C2': 'assets/lessons/c2.json',
-  };
-
-  // Future<List<LessonModel>> loadLessonsFromJson(String englishLevel) async {
-  //   final content = await rootBundle.loadString(lessonListPath[englishLevel]!);
-  //   final jsonString = jsonDecode(content.toString());
-  //   final lessonsList =
-  //       (jsonString as List).map((e) => LessonModel.fromJson(e)).toList();
-  //   return lessonsList;
-  // }
 
   void fetchUserDetails() {
     var profileData = globalController.prefs?.getString(AppStrings.userProfile);
@@ -120,6 +71,7 @@ class HomeScreenController extends GetxController {
             "${globalController.appDocDirectoryPath}/lessons/${key}_${config[key]}.zip",
             "${globalController.appDocDirectoryPath}/lessons/",
           );
+          await renameDirectory(key: key, version: config[key]);
           await File(
             "${globalController.appDocDirectoryPath}/lessons/${key}_${config[key]}.zip",
           ).delete();
@@ -184,6 +136,13 @@ class HomeScreenController extends GetxController {
       print('Unzipped to $destinationDirectory');
     } catch (e) {
       print('Error unzipping file: $e');
+    }
+  }
+
+  Future<void> renameDirectory({required String key, required String version})async{
+    final dir = Directory("${globalController.appDocDirectoryPath}/lessons/${key}_$version");
+    if (await dir.exists()) {
+      await dir.rename("${globalController.appDocDirectoryPath}/lessons/$key");
     }
   }
 }
