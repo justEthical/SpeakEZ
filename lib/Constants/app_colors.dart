2546365be2f2d67b15da0025cbd98c17d4bde997
justEthical <ammmm4507@gmail.com>
2025-09-28 17:57:39 +0530

    added poppins local font

--- lib/Constants/app_colors.dart
+++ lib/Constants/app_colors.dart
@@ -1,5 +1,7 @@
 import 'dart:ui';
 
+import 'package:flutter/material.dart';
+
 class AppColors {
 //   Primary Color: #4A90E2 (Calm Medium Blue)
 
@@ -8,7 +10,7 @@ class AppColors {
 // Accent: #FDCB6E (Warm Yellow) – For call-to-action buttons or highlights
 
 // Background: #F5F6FA (Almost white – gentle on eyes)
-  static const Color primaryColor = Color(0xFF4A90E2);
+  static const Color primaryColor = Colors.deepPurple;
   static const Color secondaryColor = Color(0xFF7ED6DF);
   static const Color accentColor = Color(0xFFFDCB6E);
   static const Color backgroundColor = Color(0xFFF5F6FA);
